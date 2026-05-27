59ddd38137cd9bd861bb53e5dbaa115b5b3a7148
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-05-27 15:37:17 +0900

    Fix:Practice3-02完了

--- spring_practice/src/main/java/jp/co/sss/practice/p03/q02/form/FavoriteForm.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p03/q02/form/FavoriteForm.java
@@ -2,4 +2,69 @@ package jp.co.sss.practice.p03.q02.form;
 
 public class FavoriteForm {
 
+	String fruit;
+	String snack;
+	String sweet;
+	String sidedish;
+
+	/**
+	 * @return fruit
+	 */
+	public String getFruit() {
+		return fruit;
+	}
+
+	/**
+	 * @param fruit
+	 *            セットする fruit
+	 */
+	public void setFruit(String fruit) {
+		this.fruit = fruit;
+	}
+
+	/**
+	 * @return snack
+	 */
+	public String getSnack() {
+		return snack;
+	}
+
+	/**
+	 * @param snack
+	 *            セットする snack
+	 */
+	public void setSnack(String snack) {
+		this.snack = snack;
+	}
+
+	/**
+	 * @return sweet
+	 */
+	public String getSweet() {
+		return sweet;
+	}
+
+	/**
+	 * @param sweet
+	 *            セットする sweet
+	 */
+	public void setSweet(String sweet) {
+		this.sweet = sweet;
+	}
+
+	/**
+	 * @return sidedish
+	 */
+	public String getSidedish() {
+		return sidedish;
+	}
+
+	/**
+	 * @param sidedish
+	 *            セットする sidedish
+	 */
+	public void setSidedish(String sidedish) {
+		this.sidedish = sidedish;
+	}
+
 }
