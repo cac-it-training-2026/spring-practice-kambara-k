9c086a16b62c4040fd85c371ccbcd6c55f322abf
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-05-27 16:18:44 +0900

    Fix:Practice4-01完了

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
@@ -1,5 +1,24 @@
 package jp.co.sss.practice.p04.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p04.q01.form.BmiForm;
+
+@Controller
 public class Practice0401Controller {
 
+	@RequestMapping(path = "/bmi/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice04/01/bmi_input";
+	}
+
+	@RequestMapping(path = "/bmi/result", method = RequestMethod.POST)
+	public String result(BmiForm bmiForm, Model model) {
+		bmiForm.setBmi(bmiForm.getWeight() / Math.pow((bmiForm.getHeight() / 100), 2));
+		model.addAttribute("bmi", bmiForm.getBmi());
+		return "practice04/01/bmi_result";
+	}
 }
