5d91a5f5514c2904beaaef4b6c79145d3c09d4fa
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-05-29 10:55:24 +0900

    Fix:Practice5-02完了

--- spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
@@ -1,5 +1,7 @@
 package jp.co.sss.practice.p05.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import jp.co.sss.practice.p05.entity.FruitsSeason;
 
 @Repository
 public interface FruitsSeasonRepository extends JpaRepository<FruitsSeason, Integer> {
+
+	List<FruitsSeason> findAllByOrderBySeasonMonth();
 
 }
